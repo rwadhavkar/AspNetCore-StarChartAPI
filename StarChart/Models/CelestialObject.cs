c55ee4a7e5b923e5fbcc2115e4cb04974f4c6055
Rohan <rwadhavkar@redventures.com>
2019-07-05 18:06:25 -0400

    created Model

--- StarChart/Models/CelestialObject.cs
+++ StarChart/Models/CelestialObject.cs
@@ -11,7 +11,7 @@ namespace StarChart.Models
         public int Id { get; set; }
         [Required]
         public String Name { get; set; }
-        public int? OrbitedObject { get; set; }
+        public int? OrbitedObjectId { get; set; }
         [NotMapped]
         public List<CelestialObject> Satellites { get; set; }
         public TimeSpan OrbitalPeriod { get; set; }
